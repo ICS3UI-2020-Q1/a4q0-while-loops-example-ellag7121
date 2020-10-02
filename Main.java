36f89ea8d3ddac93750c137112a1db88de43684e
ellag7121 <ellag7121@wrdsb.ca>
2020-10-02 14:14:48 +0000

    finished while loop example

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * while loop example
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +9,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // start counting message
+    System.out.println("Starting to count");
+
+    //make a counter variable
+    int counter = 1;
+
+    //make a loop that counts to 5
+    while(counter <= 5){
+      //print counter
+      System.out.println(counter);
+      //increase counter by 1
+      counter = counter + 1;
+    }
     
+    //finished counting message
+    System.out.println("Done counting");
   }
 }
